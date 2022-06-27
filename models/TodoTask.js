a9dc3f9cccc0bf5cc6e56947b3e4384c5dade0fa
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-06-27 06:40:37 -0400

    added all files

---
+++ models/TodoTask.js
@@ -0,0 +1,16 @@
+// THIS IS OUR SCHEMA
+const mongoose = require('mongoose');
+
+const todoTaskSchema = new mongoose.Schema({
+content: {
+type: String,
+required: true
+},
+
+date: {
+type: Date,
+default: Date.now
+}
+
+})
+module.exports = mongoose.model('TodoTask',todoTaskSchema);
